d4df0e0f0af9f85e7334b2bd25c2bc32658f214e
ivo-bass <ivailo.ignatoff@gmail.com>
2021-07-05 21:26:26 +0300

    added media files settings

--- templates_advanced/pythons_app/urls.py
+++ templates_advanced/pythons_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+
+from .. import settings
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('create/', views.create, name="create")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
